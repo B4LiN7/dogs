15adc06c9c6beb8df463cebd41c090979fd0e703
B4LiN7 <balint.k.furedi@outlook.com>
2023-10-16 17:03:06 +0200

    2. feladat

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 import dogs from "./dogs.json";
 
-// console.log(dogs);
+const table = document.getElementById("table");
+
+function drawTable(dogs_data) {
+  if (table.firstChild) {
+    table.removeChild(table.lastChild);
+  }
+
+  dogs_data.forEach(dog => {
+    table.innerHTML += `
+      <tr>
+        <td>${dog.id}</td>
+        <td>${dog.nev}</td>
+        <td>${dog.eletkor}</td>
+        <td>${dog.fajta}</td>
+        <td>${dog.gazda_neve}</td>
+      </tr>
+    `;
+  });
+}
+
+drawTable(dogs);
+
 
 console.log(
   Array.from(dogs).sort((a, b) => a.nev.localeCompare(b.nev)).sort((a, b) => a.eletkor - b.eletkor)
@@ -20,4 +41,18 @@ console.log(
   dogs.filter((dog) => dog.eletkor > 10).map(dog => `${dog.gazda_neve}`)
 );
 
+// Fajta szerint csoportosítva, a kutyák számát. Az eredményt lista helyett objektumban is tárolhatod, ekkor az objektum adattagjai a fajta nevek legyenek.
+/*
+let fajtak = []
 
+dogs.forEach(dog => {
+ if (fajtak.includes({nev: dog.fajta})) {
+  fajtak[fajtak.indexOf({nev: dog.fajta})].num++;
+ }
+ else {
+  fajtak.push({nev: dog.fajta, num: 1});
+ }
+});
+console.log(fajtak);
+
+*/
