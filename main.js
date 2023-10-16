f5262d2b581e45f65d7c9d724f8ac845821a4e90
B4LiN7 <balint.k.furedi@outlook.com>
2023-10-16 16:37:54 +0200

    1/C) feladat

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 import dogs from "./dogs.json";
 
+// console.log(dogs);
+
+console.log(
+  Array.from(dogs).sort((a, b) => a.nev.localeCompare(b.nev)).sort((a, b) => a.eletkor - b.eletkor)
+);
+
+console.log(dogs
+  .filter((dog) => dog.fajta == "Labrador")
+  .map((dog) => `${dog.nev} (${dog.eletkor} éves)`)
+);
+
+console.log(dogs
+  .filter((dog) => dog.fajta == "Labrador")
+  .map((dog) => ({nev: dog.nev, eletkor: dog.eletkor}))
+);
+
+console.log(
+  dogs.filter((dog) => dog.eletkor > 10).map(dog => `${dog.gazda_neve}`)
+);
+
+
